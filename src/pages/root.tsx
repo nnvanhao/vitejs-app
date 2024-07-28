3ad188d68b1fc6b5fc39a7928aeedecfcfa61dd5
nnvanhao <nnvanhao@gmail.com>
2024-07-28 18:23:42 +0700

    added shacdn ui

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,9 +1,35 @@
 import { Outlet } from "react-router-dom";
 
+import { Alert, AlertDescription, AlertTitle } from "@components/ui/alert";
+import { Button } from "@components/ui/button";
+import { ChartConfig, ChartContainer } from "@components/ui/chart";
+import { Terminal } from "lucide-react";
+import { Bar, BarChart } from "recharts";
+
 export default function Root() {
+  const chartData = [
+    { month: "January", desktop: 186, mobile: 80 },
+    { month: "February", desktop: 305, mobile: 200 },
+    { month: "March", desktop: 237, mobile: 120 },
+    { month: "April", desktop: 73, mobile: 190 },
+    { month: "May", desktop: 209, mobile: 130 },
+    { month: "June", desktop: 214, mobile: 140 },
+  ];
+
+  const chartConfig = {
+    desktop: {
+      label: "Desktop",
+      color: "#2563eb",
+    },
+    mobile: {
+      label: "Mobile",
+      color: "#60a5fa",
+    },
+  } satisfies ChartConfig;
+
   return (
     <div>
-      Root <Outlet />
+      <Button>Click me</Button>
     </div>
   );
 }
